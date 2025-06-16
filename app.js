8bd022305b5fa7ec92f7d03e1957f1c2828e7d0e
MayankKumarSingh1 <mayankumarsingh1243@gmail.com>
2025-06-16 14:39:42 +0530

    port

--- app.js
+++ app.js
@@ -16,10 +16,8 @@ app.use(express.urlencoded({extended: true}))
 app.use('/user',userRouter)
 
 app.use('/', indexRouter)
-// app.get('/',(req,res)=>{
-//     res.render("index")
-// })
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
 }) 
